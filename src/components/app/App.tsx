1d3e2340d2ee9cb32f4cb291d565c12e050bcff4
H Chowdhury <hassan.chowdhury1@googlemail.com>
2020-09-03 22:17:52 +0100

    create footer component and add it to app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from '../home';
 import Header from '../header';
+import Footer from '../footer';
 
 const App: React.FC = () => {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 };
